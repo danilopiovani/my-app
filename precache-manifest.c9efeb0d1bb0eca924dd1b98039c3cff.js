26e567147fc3b16bb195e7cf90d81c4d76bd684c
Danilo Piovani <danilo.piovani@gmail.com>
2020-05-12 05:00:29 +1000

    Updates

---
+++ precache-manifest.c9efeb0d1bb0eca924dd1b98039c3cff.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88ca5e051cf77a901cf9135aad9ce681",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "6acf320f7e59b5c07eae",
+    "url": "/my-app/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "b3b6e6e404d21917512e",
+    "url": "/my-app/static/css/main.bbf37a6c.chunk.css"
+  },
+  {
+    "revision": "6acf320f7e59b5c07eae",
+    "url": "/my-app/static/js/2.feec32d0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/my-app/static/js/2.feec32d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3b6e6e404d21917512e",
+    "url": "/my-app/static/js/main.0ed24d5a.chunk.js"
+  },
+  {
+    "revision": "7017a0a3783b2a9ce739",
+    "url": "/my-app/static/js/runtime-main.b51f6de3.js"
+  },
+  {
+    "revision": "9b6d2bea1a0f99ea3236da2b6403e4bf",
+    "url": "/my-app/static/media/ProdA.9b6d2bea.jpg"
+  },
+  {
+    "revision": "fb60ca0d9bedf8325b71281da9bc2601",
+    "url": "/my-app/static/media/ProdB.fb60ca0d.jpg"
+  },
+  {
+    "revision": "f2a7a4787725a8c8cbedb1c27d775bbc",
+    "url": "/my-app/static/media/ProdC.f2a7a478.jpg"
+  },
+  {
+    "revision": "2450b2c87720e693471c6b8de3bc5d25",
+    "url": "/my-app/static/media/ProdD.2450b2c8.jpg"
+  },
+  {
+    "revision": "60bc500f7bab9b214ae076a1374bdfc1",
+    "url": "/my-app/static/media/ProdE.60bc500f.jpg"
+  },
+  {
+    "revision": "c3967effc392eb482e9109834639acc5",
+    "url": "/my-app/static/media/ProdF.c3967eff.jpg"
+  },
+  {
+    "revision": "21bac4390ef4a2b97206004983c890a9",
+    "url": "/my-app/static/media/shopping.21bac439.webp"
+  },
+  {
+    "revision": "2c65e96a813b1125532a85d9bad69d99",
+    "url": "/my-app/static/media/shopping.2c65e96a.jpg"
+  },
+  {
+    "revision": "83c59c46508312847a8d6e1d9d6269da",
+    "url": "/my-app/static/media/uploadImage.83c59c46.png"
+  }
+]);
